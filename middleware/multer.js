d287bb0829dd1f7a2e5261a8dec31e04059e9bd9
yogesh <etpr.yogesh@gmail.com>
2021-10-20 01:07:23 +0530

    milter middleware

---
+++ middleware/multer.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+
+const excelFilter = (req, file, cb) => {
+  if (
+    file.mimetype.includes("excel") ||
+    file.mimetype.includes("spreadsheetml")
+  ) {
+    cb(null, true);
+  } else {
+    cb("please upload xlxs spreadsheet or excel sheet", false);
+  }
+};
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname + "/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+var uploadFile = multer({ storage: storage, fileFilter: excelFilter });
+module.exports = uploadFile;
